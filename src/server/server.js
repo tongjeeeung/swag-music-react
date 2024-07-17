82d4488f64d45ee1ea969c9661bd66a85fc8451a
Tong <tongjeeeung@gmail.com>
2024-07-17 20:22:47 +0300

    fix

--- src/server/server.js
+++ src/server/server.js
@@ -30,7 +30,7 @@ app.use(bodyParser.json());
 
 const allowedOrigins = [
   'http://localhost:5173',
-  'https://tongjeeeung.github.io'
+  'https://tongjeeeung.github.io/swag-music-react'
 ];
 
 // Используем middleware cors
