9fbe4ab6ea5fd383081c105003ed85dc31795d23
tongjeeeung <tongjeeeung@gmail.com>
2025-02-25 17:17:46 +0300

    big interface update, cookies and animations added 2

--- src/server/server.js
+++ src/server/server.js
@@ -859,9 +859,9 @@ app.delete("/blog/clear", async (req, res) => {
 //const MONGODB_URL = "mongodb+srv://tongjeeeung:<password>@swag-music.wpw2inx.mongodb.net/?retryWrites=true&w=majority&appName=swag-music"
 
 // Подключение к базе данных и запуск сервера
-mongoose.connect(process.env.MONGODB_URL)
+//mongoose.connect("mongodb://localhost:27017/musicapp")
 mongoose
-  .connect("mongodb://localhost:27017/musicapp")
+  .connect(process.env.MONGODB_URL)
   .then(() => {
     console.log("Подключено к базе данных");
     initializeDatabase();
